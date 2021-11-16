a9920de84f6b926411051fdc1f05800a774e2b2d
MrPinkPanther10 <90013822+MrPinkPanther10@users.noreply.github.com>
2021-11-16 18:27:05 -0500

    updating

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ var questionCount = 0;
 
 // Answer Buttons
 // Button in general
-const ansBtn = document.querySelectorAll("button.btns")
+const ansBtn = document.querySelectorAll("button.ansBtn")
 // Answer 1
 const ans1Btn = document.querySelector("#answer1")
 // Answer 2
@@ -34,10 +34,34 @@ const questions = [
     {
         // 1st Question
         question: "What is the name of Han Solo's Spaceship?",
-        answers: ["1. USS Enterprise - B", "2. Millenium Falcon", "3. Death Star", "4. Discovery"],
+        answers: ["USS Enterprise - B", "Millenium Falcon", "Death Star", "Discovery"],
         correctAnswer: "1"
+    },
+    {
+        // question 1
+        question: "The condition in an if / else statement is enclosed within ____.",
+        answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        correctAnswer: "1"
+    },
+    {
+        // question 2
+        question: "Arrays in Javascript can be used to store ____.",
+        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        correctAnswer: "3"
+    },
+    {
+        // question 3
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        correctAnswer: "2"
+    },
+    {
+        // question 4
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        correctAnswer: "3"
     }
-];
+]
 
 
 // FUNCTIONS DEPARTMENT
@@ -85,22 +109,21 @@ function questionStart(id) {
 }
 
 // function to check correct answer and on to the next question
-function checkAnswer(event) {
-    correct = questions[questionCount].correctAnswer;
-    event.preventDefault();
+function checkAnswer(answer) {
+    answer.preventDefault();
 
     // time out after 1 second
     setTimeout(function () {
-
+        questionStart(questionCount)
     }, 1000);
 
     // answer checker
-    if (correct === event.target.value) {
-        console.log("correct")
-    } else if (correct !== event.target.value) {
-        console.log("wrong")
+    if (questions[questionCount].correctAnswer === answer.target.value) {
+        alert("That is Correct")
+    } else if (questions[questionCount].correctAnswer !== answer.target.value) {
+        alert("That is Wrong")
     }
-
+    
     // increment so the questions index is increased
     if (questionCount < questions.length) {
         questionCount++;
@@ -110,9 +133,14 @@ function checkAnswer(event) {
 }
 
 
+
 // Event Listeners
+
+// Play the Test
 playBtn.addEventListener("click", startTest);
+// Show Records
 recordsBtn.addEventListener("click", showRecords);
+// Pick an answer
 ansBtn.forEach(item => {
     item.addEventListener('click', checkAnswer);
 });

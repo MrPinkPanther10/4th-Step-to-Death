28d36cf4d53037982beb29c2675d8c2db3651f84
MrPinkPanther10 <90013822+MrPinkPanther10@users.noreply.github.com>
2021-11-17 20:48:07 -0500

    updated button functions and scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,12 @@ var secondsLeft = 16
 // Question Section
 var questionEl = document.querySelector("#question")
 var questionCount = 0;
+// Final Score Section
+var resultEl = document.querySelector("#final")
+var finalScoreEl = document.querySelector(".score-result")
+
+// Score Array
+var scoreEl = 0;
 
 // Answer Buttons
 // Button in general
@@ -38,27 +44,27 @@ const questions = [
         correctAnswer: "1"
     },
     {
-        // question 1
-        question: "The condition in an if / else statement is enclosed within ____.",
-        answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
-        correctAnswer: "1"
-    },
-    {
-        // question 2
-        question: "Arrays in Javascript can be used to store ____.",
-        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        // 2nd Question
+        question: "What is Captain America's shield made of?",
+        answers: ["Adamantium", "Uru", "Titanium", "Vibranium"],
         correctAnswer: "3"
     },
     {
-        // question 3
-        question: "String values must be enclosed within ____ when being assigned to variables.",
-        answers: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        // 3rd Question
+        question: "Who was the first Robin in DC Comics?",
+        answers: ["Jason Todd", "Damian Wayne   ", "Dick Grayson", "Tim Drake"],
         correctAnswer: "2"
     },
     {
-        // question 4
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        // 4th Question
+        question: "S.H.I.E.L.D stands for _______",
+        answers: ["Strategic Homeland Intervention, Enforcement and Logistics Division.", "Surgical Homeland Invasion, Energy and Livestock Division", "Strategic Hostile Intervention, Endorsement and Laser Division", "Strategic Homeland Invasion, Enforcement and Logistics Division"],
+        correctAnswer: "0"
+    },
+    {
+        // 5th Question
+        question: "Which Order brought down the Jedi?",
+        answers: ["Order 69", "Order 51", "Order 99", "Order 66"],
         correctAnswer: "3"
     }
 ]
@@ -72,10 +78,10 @@ function setTime() {
         secondsLeft--;
         timerEl.textContent = `Time Left:${secondsLeft}s`;
 
+
         if (secondsLeft === 0 || questionCount === questions.length) {
             clearInterval(timerInterval);
-            introEl.style.display = "none";
-            quizEl.style.display = "block";
+            addScores()
         }
     }, 1000);
 }
@@ -96,41 +102,59 @@ function showRecords() {
     recordsEl.style.display = "block"
 }
 
+// function to reset the button colors
+function resetButton() {
+    setTimeout(function () {
+        ans1Btn.style.backgroundColor = ""
+        ans2Btn.style.backgroundColor = ""
+        ans3Btn.style.backgroundColor = ""
+        ans4Btn.style.backgroundColor = ""
+    }, 200);
+}
+
 
 // function to show questions and choices
 function questionStart(id) {
-    if (id < questions.length) {
-        questionEl.textContent = questions[id].question;
-        ans1Btn.textContent = questions[id].answers[0];
-        ans2Btn.textContent = questions[id].answers[1];
-        ans3Btn.textContent = questions[id].answers[2];
-        ans4Btn.textContent = questions[id].answers[3];
-    }
+    setTimeout(function () {
+        if (id < questions.length) {
+            questionEl.textContent = questions[id].question;
+            ans1Btn.textContent = questions[id].answers[0];
+            ans2Btn.textContent = questions[id].answers[1];
+            ans3Btn.textContent = questions[id].answers[2];
+            ans4Btn.textContent = questions[id].answers[3];
+        }
+    }, 250);
 }
 
 // function to check correct answer and on to the next question
 function checkAnswer(answer) {
     answer.preventDefault();
 
-    // time out after 1 second
-    setTimeout(function () {
-        questionStart(questionCount)
-    }, 1000);
-
     // answer checker
     if (questions[questionCount].correctAnswer === answer.target.value) {
-        alert("That is Correct")
+        answer.target.style.backgroundColor = "green"
+        scoreEl++
     } else if (questions[questionCount].correctAnswer !== answer.target.value) {
-        alert("That is Wrong")
+        answer.target.style.backgroundColor = "red"
     }
-    
+
     // increment so the questions index is increased
     if (questionCount < questions.length) {
         questionCount++;
     }
 
     questionStart(questionCount)
+    resetButton()
 }
+
+// function to add all scores and show final score section
+function addScores() {
+    quizEl.style.display = "none";
+    resultEl.style.display = "block";
+    finalScoreEl.textContent = ("You have the score of " + scoreEl + " out of " + questions.length)
+}
+
+
 
 
 
@@ -144,3 +168,17 @@ recordsBtn.addEventListener("click", showRecords);
 ansBtn.forEach(item => {
     item.addEventListener('click', checkAnswer);
 });
+
+// if (secondsLeft === 0 || questionCount === questions.length) {
+//     clearInterval(timerInterval);
+//     quizEl.style.display = "none";
+//     resultEl.style.display = "block";
+// }
+// }, 1000);
+
+// if (secondsLeft === 0 || questionCount === questions.length) {
+//     questionCount++
+//     questionStart(questionCount)
+//     clearInterval(timerInterval);
+// }
+// }, 1000);
